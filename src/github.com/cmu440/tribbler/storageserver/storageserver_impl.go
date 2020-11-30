1a8a2fedeca0620163882bc5a45b6a5067010d01
Zhengze Gong <harry8698@icloud.com>
2020-11-30 17:30:00 +0800

    backup

--- src/github.com/cmu440/tribbler/storageserver/storageserver_impl.go
+++ src/github.com/cmu440/tribbler/storageserver/storageserver_impl.go
@@ -7,11 +7,35 @@ import (
 	"github.com/cmu440/tribbler/rpc/storagerpc"
 )
 
+type serverRole string
+
+const (
+	MasterServer serverRole = "MASTER"
+	SlaveServer serverRole = "SLAVE"
+)
+
 type storageServer struct {
 	isAlive bool       // DO NOT MODIFY
 	mux     sync.Mutex // DO NOT MODIFY
 
 	// TODO: implement this!
+	role serverRole
+	serverNodes []storagerpc.Node
+	node storagerpc.Node
+
+	masterAddr string // for slave servers
+
+	port int
+	numNodes int
+	virtualIDs []uint32
+
+	readyChan chan bool
+
+	serverJoinChan chan storagerpc.Node
+	serverJoinReplyChan chan storagerpc.RegisterReply
+
+	itemStorage map[string]string
+	listStorage map[string][]string
 }
 
 // USED FOR TESTS, DO NOT MODIFY
@@ -36,38 +60,244 @@ func NewStorageServer(masterServerHostPort string, numNodes, port int, virtualID
 	/****************************************************************************/
 
 	// TODO: implement this!
+	if masterServerHostPort == nil || masterServerHostPort == "" {
+		ss.role = MasterServer
+		masterAddr = nil
+	} else {
+		ss.role = SlaveServer
+		masterAddr = masterServerHostPort
+	}
 
-	return nil, errors.New("not implemented")
+	ss.node := Node { HostPort: 'localhost:' + strconv(port), virtualIDs: virtualIDs }
+	ss.itemStorage = make(map[string]string)
+	ss.listStorage = make(map[string][]string)
+	
+	if ss.role == MasterServer {
+		ss.mux.Lock()
+		ss.serverNodes = append(ss.serverNodes, node)
+		ss.mux.Unlock()
+	} else {
+		// ignore for checkpoint
+	}
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+    if err != nil {
+        return nil, err
+    }
+
+	err = rpc.RegisterName("StorageServer", storagerpc.Wrap(ss))
+    if err != nil {
+        return nil, err
+	}
+	
+	rpc.HandleHTTP()
+    go http.Serve(listener, nil)
+
+	if ss.role == MasterServer {
+		ss.mux.Lock()
+		ss.serverNodes = append(ss.serverNodes, storagerpc.Node { 
+			HostPort: 'localhost:' + strconv(port), 
+			virtualIDs: virtualIDs, 
+		})
+		ss.mux.Unlock()
+
+		for {
+			newNode := <- ss.serverJoinChan
+
+			ss.mux.Lock()
+			exists := false
+			for _, n in range(ss.serverNodes) {
+				if n == newNode {
+					exists = true
+				}
+			}
+			if !exists {
+				ss.serverNodes = append(ss.serverNodes, newNode)
+			}
+
+			if len(ss.serverNodes) == ss.numNodes {
+				ss.serverJoinReplyChan <- storagerpc.RegisterReply {
+					Status: storagerpc.OK,
+					Servers: ss.serverNodes,
+				}
+				ss.mux.Unlock()
+				break
+			}
+			 
+			ss.serverJoinReplyChan <- storagerpc.RegisterReply {
+				Status: storagerpc.NotReady,
+			}
+			ss.mux.Unlock()
+		}
+	} else {
+		// ignored for checkpoint
+	}
+
+	err := nil
+	return ss, err
 }
 
 func (ss *storageServer) registerServer(args *storagerpc.RegisterArgs, reply *storagerpc.RegisterReply) error {
-	return errors.New("not implemented")
+	ss.mux.Lock()
+	defer ss.mux.Unlock()
+	if ss.role == SlaveServer {
+		return errors.New("request sent to slave server")
+	} 
+	
+	if len(ss.serverNodes) < ss.numNodes {
+		server := args.ServerInfo
+		ss.serverJoinChan <- server
+		reply = <- ss.serverJoinReplyChan
+	} else {
+		reply.Status = storagerpc.OK
+		reply.Servers = ss.serverNodes
+	}
 }
 
 func (ss *storageServer) getServers(args *storagerpc.GetServersArgs, reply *storagerpc.GetServersReply) error {
-	return errors.New("not implemented")
+	ss.mux.Lock()
+	defer ss.mux.Unlock()
+	if len(ss.serverNodes) < ss.numNodes {
+		reply.Status = storagerpc.NotReady
+	} else {
+		reply.Servers = ss.serverNodes
+	}
+}
+
+func findStorageServer(ss *storageServer, key string) storagerpc.Node {
+	hash := libstore.StoreHash(string.split(key, ':')[0])
+	var server storagerpc.Node
+	var nearestID uint32 = ^uint32(0)
+
+	for _, n := range(ss.serverNodes) {
+		for _, id := range(n.virtualIDs) {
+			if id >= hash && id < nearestID {
+				nearestID = id
+				server = n
+			}
+		}
+	}
+
+	return server
 }
 
 func (ss *storageServer) get(args *storagerpc.GetArgs, reply *storagerpc.GetReply) error {
-	return errors.New("not implemented")
+	ss.mux.Lock()
+	defer ss.mux.Unlock()
+
+	if ss.node != findStorageServer(ss, args.Key) {
+		return errors.New("key doesn't belong to this server")
+	}
+
+	value, found := ss.itemStorage[args.Key]
+	if !found {
+		reply.Status = storagerpc.KeyNotFound
+	} else {
+		reply.Status = storagerpc.OK
+		reply.Value = value
+	}
+
+	return nil
 }
 
 func (ss *storageServer) delete(args *storagerpc.DeleteArgs, reply *storagerpc.DeleteReply) error {
-	return errors.New("not implemented")
+	ss.mux.Lock()
+	defer ss.mux.Unlock()
+
+	if ss.node != findStorageServer(ss, args.Key) {
+		return errors.New("key doesn't belong to this server")
+	}
+
+	value, found := ss.itemStorage[args.Key]
+	if !found {
+		reply.Status = storagerpc.KeyNotFound
+	} else {
+		reply.Status = storagerpc.OK
+		delete(ss.storage, args.Key)
+	}
+
+	return nil
 }
 
 func (ss *storageServer) getList(args *storagerpc.GetArgs, reply *storagerpc.GetListReply) error {
-	return errors.New("not implemented")
+	ss.mux.Lock()
+	defer ss.mux.Unlock()
+
+	if ss.node != findStorageServer(ss, args.Key) {
+		return errors.New("key doesn't belong to this server")
+	}
+
+	value, found := ss.listStorage[args.Key]
+	if !found {
+		reply.Status = storagerpc.KeyNotFound
+	} else {
+		reply.Status = storagerpc.OK
+		reply.Value = value
+	}
+
+	return nil
 }
 
 func (ss *storageServer) put(args *storagerpc.PutArgs, reply *storagerpc.PutReply) error {
-	return errors.New("not implemented")
+	ss.mux.Lock()
+	defer ss.mux.Unlock()
+
+	if ss.node != findStorageServer(ss, args.Key) {
+		return errors.New("key doesn't belong to this server")
+	}
+
+	ss.itemStorage[args.Key] = args.Value
+	reply.Status = storagerpc.OK
+
+	return nil
 }
 
 func (ss *storageServer) appendToList(args *storagerpc.PutArgs, reply *storagerpc.PutReply) error {
-	return errors.New("not implemented")
+	ss.mux.Lock()
+	defer ss.mux.Unlock()
+
+	if ss.node != findStorageServer(ss, args.Key) {
+		return errors.New("key doesn't belong to this server")
+	}
+
+	value, found := ss.listStorage[args.Key]
+	reply.Status = storagerpc.OK
+
+	for _, v := range(value) {
+		if v == args.Value {
+			reply.Status = storagerpc.ItemExists
+			break
+		}
+	}
+	ss.listStorage[args.Key] = append(value, args.Value)
+
+	return nil
 }
 
 func (ss *storageServer) removeFromList(args *storagerpc.PutArgs, reply *storagerpc.PutReply) error {
-	return errors.New("not implemented")
+	ss.mux.Lock()
+	defer ss.mux.Unlock()
+
+	if ss.node != findStorageServer(ss, args.Key) {
+		return errors.New("key doesn't belong to this server")
+	}
+
+	value, found := ss.listStorage[args.Key]
+	if !found {
+		reply.Status = storagerpc.KeyNotFound
+	} else {
+		newList := nil
+		for _, v := range(value) {
+			if v != args.Value { 
+				newList = append(newList, v)
+			}
+		}
+		if len(newList) == len(value) {
+			reply.Status = storagerpc.ItemNotFound
+		} else {
+			reply.Status = storagerpc.OK
+		}
+	}
+
+	return nil
 }
--- src/github.com/cmu440/tribbler/tribserver/tribserver_impl.go
+++ src/github.com/cmu440/tribbler/tribserver/tribserver_impl.go
@@ -1,14 +1,118 @@
 package tribserver
 
 import (
+	"container/heap"
 	"errors"
-
+	"github.com/cmu440/tribbler/rpc/libstore"
+	"math"
+	"log"
+	"strings"
+	"time"
+	"strconv"
+	"encoding/json"
+	"net/rpc"
 	"github.com/cmu440/tribbler/rpc/tribrpc"
 )
 
-type tribServer struct {
-	// TODO: implement this!
+
+//*****************************Implementation of a Priority Queue*******************
+type pqElem struct {
+	index  int 
+	timeStamp  int
+	source int
+	user  string
 }
+
+type Queue []*pqElem
+
+func (pq Queue) Len() int {
+	return len(pq)
+}
+
+func (pq Queue) Less(i, j int) bool {
+	return pq[i].timeStamp > pq[j].timeStamp
+}
+
+func (pq Queue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *Queue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*pqElem)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *Queue) Pop() interface{} {
+	current := *pq
+	n := len(current)
+	item := current[n-1]
+	item.index = -1
+	current[n-1] = nil 
+	*pq = current[0 : n-1]
+	return item
+}
+
+func (pq *Queue) update(item *pqElem, userID string, timeStamp int) {
+	item.user = userID
+	item.timeStamp = timeStamp
+	heap.Fix(pq, item.index)
+}
+
+type tribServer struct {
+	listener net.Listener
+	store libstore.Libstore
+}
+
+
+
+const (
+	TribbleMax = 100
+)
+
+func generateUserID (user string) string {
+	var userToken string
+	userToken = ":user"
+	return user+userToken
+}
+
+func generateUserSubKey (user string) string {
+	var subToken string
+	subToken = ":subsription"
+	return user+subToken
+}
+
+func generateTribbleID (user string, currentTime time.Time) string {
+	var tribbleToken string
+	tribbleToken = ":subsription"
+	return user + tribbleToken + ":" + strconv.FormatInt(currentTime.UnixNano(), 10)
+}
+
+func generateUserTribbleID (user string) string {
+	var tribbleToken string
+	tribbleToken = ":tribble"
+	return user+tribbleToken
+}
+
+func parseTribble (tribbleInfo string) (string, int) {
+	ls := strings.Split(tribbleInfo, ":")
+	timeStamp := strconv.Atoi(ls[2])
+	return  ls[0], timeStamp
+}
+
+
+func checkInList (elem string, subscribers []string) bool {
+	for j:=0; j<len(subscribers); j++ {
+		if elem == subscribers[j] {
+			return true 
+		}
+	}
+	return false
+}
+
 
 // NewTribServer creates, starts and returns a new TribServer. masterServerHostPort
 // is the master storage server's host:port and port is this port number on which
@@ -17,37 +121,301 @@ type tribServer struct {
 //
 // For hints on how to properly setup RPC, see the rpc/tribrpc package.
 func NewTribServer(masterServerHostPort, myHostPort string) (TribServer, error) {
-	return nil, errors.New("not implemented")
+	store, err := libstore.NewLibstore(masterServerHostPort, myHostPort, libstore.Normal)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the server socket that will listen for incoming RPCs.
+	listener, err := net.Listen("tcp", myHostPort)
+	if err != nil {
+		return nil, err
+	}
+	ts := &tribServer {
+		listener: listener,
+		store: store
+	}
+	if err = rpc.RegisterName("TribServer", tribrpc.Wrap(ts)) {
+		if err != nil {
+			return nil, err
+		}
+	}
+	rpc.HandleHTTP()
+	go http.Serve(ts.listener, nil)
+	return ts, nil
 }
 
 func (ts *tribServer) CreateUser(args *tribrpc.CreateUserArgs, reply *tribrpc.CreateUserReply) error {
-	return errors.New("not implemented")
+	newUser := generateUserID(args.UserID)
+	_, err := ts.store.Get(newUser)
+	if err != nil {
+		addErr := ts.store.Put(newUser, "")
+		if (addErr == nil) {
+			reply.Status = tribrpc.OK
+			return nil
+		} else {
+			return addErr
+		}
+	} else {
+		reply.Status = tribrpc.OK
+		return nil
+	}
 }
 
 func (ts *tribServer) AddSubscription(args *tribrpc.SubscriptionArgs, reply *tribrpc.SubscriptionReply) error {
-	return errors.New("not implemented")
+	userInfo := generateUserID(args.UserID)
+	_, err := ts.store.Get(userInfo)
+	if (err != nil) {
+		reply.Status = tribrpc.NoSuchUser
+		return nil
+	} else {
+		targetUser := generateUserID(args.TargetUserID)
+		_, targetErr := ts.store.Get(targetUser)
+		if targetErr != nil {
+			reply.Status = tribrpc.NoSuchTargetUser
+			return nil
+		} else {
+			userSubKey := generateUserSubKey(args.UserID)
+			appendErr := ts.store.AppendToList(userSubKey, targetUser)
+			if appendErr != nil {
+				reply.Status = tribrpc.Exists
+			} else {
+				reply.Status = tribrpc.OK
+			}
+			return nil
+		}
+	}
 }
 
 func (ts *tribServer) RemoveSubscription(args *tribrpc.SubscriptionArgs, reply *tribrpc.SubscriptionReply) error {
-	return errors.New("not implemented")
+	userInfo := generateUserID(args.UserID)
+	_, err := ts.store.Get(userInfo)
+	if (err != nil) {
+		reply.Status = tribrpc.NoSuchUser
+		return nil
+	} else {
+		targetUser := generateUserID(args.TargetUserID)
+		_, targetErr := ts.store.Get(targetUser)
+		if targetErr != nil {
+			reply.Status = tribrpc.NoSuchTargetUser
+			return nil
+		} else {
+			userSubKey := generateUserSubKey(args.UserID)
+			removeErr := ts.store.RemoveFromList(userSubKey, targetUser)
+			if removeErr != nil {
+				reply.Status = tribrpc.NoSuchTargetUser 
+			} else {
+				reply.Status = tribrpc.OK
+			}
+			return nil
+		}
+	}
+}
+
+func (ts *tribServer) findSubscribers(userID) ([]string, error, bool) {
+	userInfo := generateUserID(userID)
+	_, err := ts.store.Get(userInfo)
+	if err != nil {
+		return nil, nil, false
+	} else {
+		userSubKey := generateUserSubKey(userID)
+		subscribers, findErr := ts.store.GetList(userSubKey)
+		if findErr != nil {
+			return nil, findErr, false
+		} else {
+			return subscribers, nil, true
+		}
+	}
 }
 
 func (ts *tribServer) GetFriends(args *tribrpc.GetFriendsArgs, reply *tribrpc.GetFriendsReply) error {
-	return errors.New("not implemented")
+	subscribers, err, valid:= ts.findSubscribers(args.UserID)
+	res := []string{}
+	if err != nil {
+		return err
+	} else if valid == false {
+		reply.Status = tribrpc.NoSuchUser
+		return nil
+	} else {
+		userInfo := generateUserID(args.UserID)
+		for i:= 0; i<len(subscribers); i++ {
+			subID := subscribers[i]
+			subsubscribers, subErr, valid := ts.findSubscribers(subID)
+			if subErr != nil || valid == false{
+				continue
+			} else {
+				isFriend := checkInList(args.UserID, subsubscribers)
+				if isFriend {
+					res = append(res, subID)
+				}
+			}
+		}
+	}
+	return res
 }
 
 func (ts *tribServer) PostTribble(args *tribrpc.PostTribbleArgs, reply *tribrpc.PostTribbleReply) error {
-	return errors.New("not implemented")
+	userInfo := generateUserID(userID)
+	_, err := ts.store.Get(userInfo)
+	if (err != nil) {
+		reply.Status = tribrpc.NoSuchUser
+		return nil
+	} else {
+		currentTime := time.Now()
+		userTribbleID := generateUserTribbleID(args.UserID)
+		tribbleTimeID := generateTribbleID(args.UserID,currentTime)
+		newTribble := &tribrpc.Tribble {
+			UserID: args.UserID,
+			Posted: currentTime,
+			Contents: args.Contents,
+		}
+		marshalledTribble, marshErr := json.Marshal(newTribble)
+		if marshErr == nil {
+			putErr := ts.store.Put(tribbleTimeID, string(marshalledTribble))
+			if (putErr != nil) {
+				return putErr
+			} 
+			appendErr := ts.store.AppendToList(userTribbleID,tribbleTimeID)
+			if appendErr == nil {
+				reply.PostKey = tribbleTimeID
+				reply.Status = tribrpc.OK
+				return nil
+			} else {
+				return appendErr
+			}
+		} else {
+			return marshErr
+		}
+	}
 }
 
 func (ts *tribServer) DeleteTribble(args *tribrpc.DeleteTribbleArgs, reply *tribrpc.DeleteTribbleReply) error {
-	return errors.New("not implemented")
+	userInfo := generateUserID(userID)
+	_, err := ts.store.Get(userInfo)
+	if (err != nil) {
+		reply.Status = tribrpc.NoSuchUser
+		return nil
+	} else {
+		userTribbleID := generateUserTribbleID(args.UserID)
+		removeErr := ts.store.RemoveFromList(userTribbleID, args.PostKey)
+		if (removeErr != nil) {
+			return removeErr
+		}
+		deleteErr := ts.store.Delete(args.PostKey)
+		reply.Status = tribrpc.OK
+		return nil
+	}
 }
 
 func (ts *tribServer) GetTribbles(args *tribrpc.GetTribblesArgs, reply *tribrpc.GetTribblesReply) error {
-	return errors.New("not implemented")
+	userInfo := generateUserID(userID)
+	_, err := ts.store.Get(userInfo)
+	if (err != nil) {
+		reply.Status = tribrpc.NoSuchUser
+		return nil
+	} else {
+		userTribbleID := generateUserTribbleID(args.UserID)
+		tribSubscribers, listErr := ts.store.GetList(userTribbleID)
+		if listErr != nil {
+			return listErr
+		} else {
+			var tribbles [TribbleMax]tribrpc.Tribble
+			tribbleCount := 0
+			for i := len(tribSubscribers) - 1; i >= 0 ; i-- {
+				tmp := tribSubscribers[i]
+				var tribble tribrpc.Tribble
+				marshalledTribble, getErr := ts.store.Get(tmp)
+				if getErr != nil {
+					continue
+				} else {
+					json.Unmarshal(([]byte)marshalledTribble), &tribble)
+					tribbles[tribbleCount] = tribble
+					tribbleCount++
+					if (tribbleCount >= TribbleMax) {
+						break
+					}
+				}
+			}
+			reply.Tribbles = tribbles
+			reply.Status = tribrpc.OK
+			return nil
+		}
+	}
 }
 
 func (ts *tribServer) GetTribblesBySubscription(args *tribrpc.GetTribblesArgs, reply *tribrpc.GetTribblesReply) error {
-	return errors.New("not implemented")
+	userInfo := generateUserID(args.UserID)
+	_, err = ts.storage.Get(userInfo)
+	if (err != nil) {
+		reply.Status = tribrpc.NoSuchUser
+		return nil
+	} else {
+		userTribbleID := generateUserTribbleID(args.UserID)
+		var subList []string
+		if subList, getErr = ts.storage.GetList(userTribbleID) {
+			if (getErr != nil) {
+				return getErr
+			} else if len(subList) == 0 {
+				reply.Status = tribrpc.OK
+				reply.Tribbles = nil
+				return nil
+			} else {
+				var pq Queue
+				var tribList [][]string
+				for i:= 0; i<len(subList); i++ {
+					subUserID := subList[i]
+					subUserIDTrib := generateUserTribbleID(subUserID)
+					subUserList, listErr := ts.store.GetList(subUserIDTrib)
+					if (listErr != nil) {
+						return listErr
+					} else {
+						tribList = append(tribList, subUserList)
+					}
+				}
+				tribCount := 0
+				for j:= 0; j<len(subList); j++ {
+					if (len(tribList[j]) == 0) {
+						continue
+					} else {
+						listLen := len(tribList[j])
+						latestTrib := tribList[j][listLen-1]
+						_,timeStamp :=  parseTribble(latestTrib)
+						newElem := &pqElem {
+							timeStamp : timeStamp,
+							user: latestTrib,
+							source: j,
+						}
+						heap.Push(&pq, newElem)
+						tribList[j] = tribList[j][:listLen-1]
+					}
+				}
+				res := []tribrpc.Tribble{}
+				for (len(res) < TribbleMax && pq.Len()>0) {
+					elem := heap.Pop(&pq).(*pqElem)
+					var newTribble tribrpc.Tribble
+					marshalledTribble, getErr := ts.store.Get(elem.user)
+					if (getErr == nil) {
+						json.Unmarshal(([]byte)marshalledTribble, &newTribble)
+						res = append(res, newTribble)
+					}
+					idx := elem.source
+					if idx<len(tribList) && (tribList[idx]) > 0 {
+						currentLen := len(tribList[idx])
+						nextTrib := tribList[idx][currentLen-1]
+						_,timeStamp :=  parseTribble(nextTrib)
+						newElem := &pqElem {
+							timeStamp : timeStamp,
+							user: nextTrib,
+							source: idx,
+						}
+						heap.Push(&pq, newElem)
+						tribList[idx] = tribList[idx][:currentLen-1]
+					}
+				}
+				reply.Status = tribrpc.OK
+				reply.Tribbles = res
+				return nil
+			}
+		}
+	}
 }
